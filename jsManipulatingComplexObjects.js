5ca169fc9705ec9ca64996c27c2c11f41a2271df
24emmory <67297971+24emmory@users.noreply.github.com>
2020-12-26 16:36:03 -0800

    Create jsManipulatingComplexObjects.js

---
+++ jsManipulatingComplexObjects.js
@@ -0,0 +1,28 @@
+var myMusic = [
+
+
+
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+  
+    {
+      "artist":"Akira Yamaoka",
+      "title":"Silent Hill 2",
+      "release_year": 2001,
+      "formats": [
+        "CD",
+        "mp3",
+        "Vinyl",
+        "LP"
+      ]
+    }
+  ];
