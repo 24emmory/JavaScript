63b70146637f6bdb9093bcdbd957d9b153621b5f
24emmory <67297971+24emmory@users.noreply.github.com>
2021-01-13 02:43:12 -0800

    Create jsES6DestructureAssignmentAssignVariables.js
    This process basically reads:
    
    const newKey(variable) = OBJECT.oldKey
    OBJECT.oldKey used to access oldKey value
    
    :Take the value from this property (dot notation) and assign it to this new key(variable) -- destructuring, taking it out

---
+++ jsES6DestructureAssignmentAssignVariables.js
@@ -0,0 +1,16 @@
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const { today:highToday, tomorrow:highTomorrow } = HIGH_TEMPERATURES;
+  
+  //const highToday = HIGH_TEMPERATURES.today;
+  //const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+  //remove lines 11 and 12 to pass the tests, it does not want ES5 assignment syntax
+  
+
+  // Only change code above this line
