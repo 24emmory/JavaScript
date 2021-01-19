8e6fb0d026e003fd7df8082d96015ee3b460637a
24emmory <67297971+24emmory@users.noreply.github.com>
2021-01-19 09:55:16 -0800

    Create ES6UseXportShareCodeBlock.js
    We are able to use the export keyword in two methods to export code blocks to other functions. One being before each line, or at the bottom of the desired code blocks with curly brackets
    
    export ( x,y};

---
+++ ES6UseXportShareCodeBlock.js
@@ -0,0 +1,21 @@
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+  
+  export {uppercaseString,lowercaseString}
+
+  //or
+
+  export const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  export const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+  
+  
